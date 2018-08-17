69e020bf093ca7301218132045f07a4ffa6787ff
Thada Wangthammang <mildronize@gmail.com>
2018-08-17 09:58:24 +0700

    fix indent size to 2

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,11 @@
 import Layout from '../components/MyLayout.js'
 import fetch from 'isomorphic-unfetch'
 
-const Post =  (props) => (
-    <Layout>
-       <h1>{props.data.title}</h1>
-       <p>{props.data.body}</p>
-    </Layout>
+const Post = (props) => (
+  <Layout>
+    <h1>{props.data.title}</h1>
+    <p>{props.data.body}</p>
+  </Layout>
 )
 
 Post.getInitialProps = async function (context) {
