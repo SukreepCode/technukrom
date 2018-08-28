782afccec787bbc0d959fa53869959742ac8b6f2
Thada Wangthammang <mildronize@gmail.com>
2018-08-28 09:49:49 +0700

    change function from pagination to load more (append content instead of change page), refactor firestore, loading button,

--- pages/index.js
+++ pages/index.js
@@ -4,94 +4,120 @@ import dateFormat from 'dateformat';
 import React from 'react';
 import PostItem from '../components/PostItem';
 import Document from 'next/document'
-import Pagination from '../components/Pagination';
+// import Pagination from '../components/Pagination';
 
-const NUM_DATA = 14;
+const NUM_DATA = 20;
 
 export default class Index extends Document {
 
   constructor(props) {
     super(props)
     this.state = {
-      posts: createEmptyPosts()
+      posts: this.createEmptyPosts(NUM_DATA),
+      refNext: null,
+      refQuery: null,
+      isLoading: true,
     };
   }
 
-  async getData(page) {
-    const db = await firebaseInit()
-    let data = []
-    let querySnapshot
-    const settings = {/* your settings... */ timestampsInSnapshots: true };
-    db.settings(settings);
-    try {
-      let first = db.collection("posts").orderBy("published", "desc").limit(NUM_DATA)
-      if (page == 1)
-        querySnapshot = await first.get()
-      else {
-        querySnapshot = await first.get().then(function (documentSnapshots) {
-          // Get the last visible document
-          var lastVisible = documentSnapshots.docs[documentSnapshots.docs.length - page];
-          console.log("last", lastVisible);
+  handleQuery(ref, isClear) {
+    // Fetch query of given reference
+    return new Promise((resolve, reject) => {
+      ref.get().then((documentSnapshots) => {
+        this.setState({ isLoading: false });
 
-          // Construct a new query starting at this document,
-          var next = db.collection("posts").orderBy("published", "desc").startAfter(lastVisible).limit(NUM_DATA)
-          return next.get();
+        // If documentSnapshots is empty, then we have loaded all
+        if (documentSnapshots.empty) {
+          resolve(documentSnapshots);
+        };
+
+        let i = 0;
+        if (isClear) this.setState({ posts: [] });
+
+        documentSnapshots.forEach((doc) => {
+
+          this.state.posts.push({
+            "post_index": i++,
+            "id": doc.id,
+            "title": doc.data().title,
+            "link": doc.data().link,
+            "author": doc.data().author,
+            "published": dateFormat(doc.data().published.toDate(), "mediumDate"),
+            "isLoading": false
+          });
         });
+
+        // Build a reference for next page
+        const lastVisible = documentSnapshots.docs[documentSnapshots.size - 1];
+        if (!lastVisible) return;
+        this.setState({ refNext: ref.startAfter(lastVisible) })
+
+        resolve(documentSnapshots);
+
+      })
+    })
+  }
+
+  getData() {
+    try {
+      if (this.state.refNext === null) {
+        // clear state.posts for loading animation
+        // Perform query from first query (first visible)
+        this.handleQuery(this.state.refQuery.limit(NUM_DATA), true);
+      } else {
+        // Perform query from last visible page
+        this.handleQuery(this.state.refNext.limit(NUM_DATA), false);
       }
 
-      let i = 0;
-      querySnapshot.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data().published.toDate()}`);
-        data.push({
-          "post_index": i++,
-          "id": doc.id,
-          "title": doc.data().title,
-          "link": doc.data().link,
-          "author": doc.data().author,
-          "published": dateFormat(doc.data().published.toDate(), "mediumDate"),
-          "isLoading": false
-        });
-      })
     } catch (e) {
       console.log('something wrong ', e);
     }
-
-    // console.log(`Show data fetched. Count: ${data.length}`)
-    this.setState({ posts: data })
   }
+
+  async initQuery() {
+    const db = await firebaseInit()
+    const settings = {/* your settings... */ timestampsInSnapshots: true };
+    db.settings(settings);
+    this.setState({ refQuery: db.collection("posts").orderBy("published", "desc") });
+  }
+
   async componentDidMount() {
-    await this.getData(this.props.page);
+    await this.initQuery();
+    this.getData();
+  }
+
+  async loadMore() {
+    this.setState({ isLoading: true });
+    this.getData();
+    // e.preventDefault();
   }
 
   render() {
+
     return (
 
       <Base title="Technukrom">
         <h1>Recent Posts</h1>
-        <Pagination page={this.props.page} />
-
         {this.state.posts.map((post) => (
           <PostItem post={post} />
         ))}
-        <Pagination page={this.props.page} />
+        {/* <Pagination page={this.props.page} /> */}
+        <center><a class={`button is-primary ${this.state.isLoading ? "is-loading" : ""}`} onClick={this.loadMore.bind(this)}>Load more</a></center>
       </Base>
     )
   }
 
   static async getInitialProps({ pathname, query }) {
-    // const feed = pathname === '/' ? '/news' : pathname
-    console.log(query.page);
     const page = query.page === undefined ? 1 : query.page;
-    console.log(page);
     return { page: page };
   }
-}
 
-export function createEmptyPosts() {
-  let posts = []
-  for (var i = 0; i < NUM_DATA; i++) {
-    posts.push({ "isLoading": true })
+  createEmptyPosts(numData) {
+    let posts = []
+    for (var i = 0; i < numData; i++) {
+      posts.push({ "isLoading": true })
+    }
+    return posts
   }
-  return posts
-}
 
+}
