a8056539ae56dfa2eb790da48cbf2a135c637b49
Thada Wangthammang <mildronize@gmail.com>
2018-08-30 11:18:00 +0700

    add draft pagination on URL concept (still buggy)

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,11 @@ import dateFormat from 'dateformat';
 import React from 'react';
 import PostItem from '../components/PostItem';
 import Document from 'next/document'
+import Link from 'next/link'
 // import Pagination from '../components/Pagination';
 
 const NUM_DATA = 20;
+const PAGE_URL = '/#/page/'
 
 export default class Index extends Document {
 
@@ -18,15 +20,34 @@ export default class Index extends Document {
       refNext: null,
       refQuery: null,
       isLoading: true,
+      page: 1,
     };
     this.loadMore = this.loadMore.bind(this);
+
   }
 
-  handleQuery(ref, isFirstLoad, isPrefetch = false) {
+  handleQueryPagination(ref, isModifyPlaceholder = true, isPrefetch = false, page = 1, isPaginated) {
+    console.log(page);
+    if (isModifyPlaceholder) {
+      console.log(`First Load page: ${page}`);
+      if (page == 1)
+        this.handleQuery(ref, isModifyPlaceholder, isPrefetch, page);
+      else {
+        this.handleQuery(ref, isModifyPlaceholder, isPrefetch, page)
+        return this.handleQueryPagination(ref, isModifyPlaceholder, isPrefetch, page - 1);
+      }
+    } else {
+      console.log(`Load more page: ${page}`);
+      this.handleQuery(ref, isModifyPlaceholder, isPrefetch, page);
+    }
+    this.setState({ isLoading: false });
+  }
+
+  handleQuery(ref, isModifyPlaceholder = true, isPrefetch = false, page = 1) {
     // Fetch query of given reference
     return new Promise((resolve, reject) => {
       ref.get().then((documentSnapshots) => {
-        this.setState({ isLoading: false });
+        // this.setState({ isLoading: false });
 
         // If documentSnapshots is empty, then we have loaded all
         if (documentSnapshots.empty) {
@@ -54,13 +75,17 @@ export default class Index extends Document {
         if (isPrefetch)
           this.setState({ prefetch_posts: tmp_data })
         else {
-          if (isFirstLoad) {
+          if (page == 1) {
+            console.log("Hey");
             for (var j = 0; j < NUM_DATA; j++) {
               this.state.posts[j] = tmp_data[j];
             }
             // this.setState({ posts: this.state.posts.concat(tmp_data) })
-          } else
+          }else {
+            // Load data to view
+            console.log(tmp_data);
             this.setState({ posts: this.state.posts.concat(tmp_data) })
+          }
         }
 
         // Build a reference for next page
@@ -74,7 +99,8 @@ export default class Index extends Document {
     })
   }
 
-  getData() {
+  getData(isFirstLoad) {
+
     try {
 
       if (this.state.prefetch_posts) {
@@ -83,17 +109,17 @@ export default class Index extends Document {
       } else {
         // No prefetch data, then get current page
         if (this.state.refNext === null) {
-          // clear state.posts for loading animation
           // Perform query from first query (first visible)
-          this.handleQuery(this.state.refQuery.limit(NUM_DATA), true);
+          this.handleQueryPagination(this.state.refQuery.limit(NUM_DATA), true, false, this.state.page, isFirstLoad);
         } else {
           // Perform query from last visible page
-          this.handleQuery(this.state.refNext.limit(NUM_DATA), false);
+          this.handleQueryPagination(this.state.refNext.limit(NUM_DATA), false, false, this.state.page, isFirstLoad);
         }
       }
 
       // Finish? then, call prefetch
-      this.handleQuery(this.state.refNext.limit(NUM_DATA), false, true);
+      if (this.state.refNext !== null)
+        this.handleQuery(this.state.refNext.limit(NUM_DATA), false, true);
 
     } catch (e) {
       console.log('something wrong ', e);
@@ -102,19 +128,31 @@ export default class Index extends Document {
 
   async initQuery() {
     const db = await firebaseInit()
-    const settings = {/* your settings... */ timestampsInSnapshots: true };
+    const settings = { timestampsInSnapshots: true };
     db.settings(settings);
     this.setState({ refQuery: db.collection("posts").orderBy("published", "desc") });
   }
 
   async componentDidMount() {
+    const href = window.location.href;
+    const search = href.search(PAGE_URL)
+    let page = 1;
+    if (search >= 0) {
+      try {
+        page = parseInt(href.substring(search + PAGE_URL.length, href.length))
+      } catch (e) { }
+    }
+    this.setState({ page: page })
     await this.initQuery();
-    this.getData();
+    this.getData(false);
   }
 
   async loadMore() {
+    console.log(window.location.href);
+    // window.location.pathname = window.location.pathname + "\#/page";
     this.setState({ isLoading: true });
-    this.getData();
+    this.setState({ page: this.state.page + 1 })
+    this.getData(true);
     // e.preventDefault();
   }
 
@@ -127,7 +165,11 @@ export default class Index extends Document {
           <PostItem post={post} />
         ))}
         {/* <Pagination page={this.props.page} /> */}
-        <center><a class={`button is-primary ${this.state.isLoading ? "is-loading" : ""}`} onClick={this.loadMore}>ดูเพิ่มเติม</a></center>
+        <center>
+          <Link href={`${PAGE_URL}${this.state.page + 1}`}>
+            <a class={`button is-primary ${this.state.isLoading ? "is-loading" : ""}`} onClick={this.loadMore}>ดูเพิ่มเติม</a>
+          </Link>
+        </center>
       </Base>
     )
   }
