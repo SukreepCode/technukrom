786becb19cc7e5828365ca57b9232b46c99c6491
Thada Wangthammang <mildronize@gmail.com>
2018-08-30 18:38:06 +0700

    edit footer content `ทำด้วย ❤️`

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -1,13 +1,18 @@
-import { FaGithub } from 'react-icons/fa';
-
 const Footer = (props) =>
   <footer class="footer">
     <div class="content has-text-centered is-size-6">
       {/* <div>Copyright © 2018 by TechNukrom</div> */}
-      <div class="is-size-7">Powered by <a href="https://nextjs.org/">NextJS</a></div>
-      <div class="box user-link is-size-5">
-        <a class="user-link" href="https://github.com/TechNukrom/"><FaGithub/></a>
-      </div>
+      ทำด้วย {' '}
+      <span role="img" aria-label="love">
+        ❤️
+      </span>{' '}
+      และ{' '}
+      <a href="https://nextjs.org/">
+      Next.js{' '}
+        <span role="img" aria-label="with extra sparkles">
+          ✨
+        </span>
+      </a>
     </div>
   </footer>
 
