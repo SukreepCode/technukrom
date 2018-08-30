dcb10c7a22a1b286bb0e17339070eafa8619f978
Thada Wangthammang <mildronize@gmail.com>
2018-08-30 16:49:54 +0700

    add icons

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,40 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
+const theme_color = "#0D47A1";
+const description = "Techนุกรม = ศูนย์กลางความรู้สำหรับนักพัฒนา ( developer) โดยที่เน้นภาษาไทยเป็นหลัก"
+
 export default class MyDocument extends Document {
   render() {
     return (
-      <html class="has-navbar-fixed-top">
-        <Head>
+      <html class="has-navbar-fixed-top" >
+      <Head>
         <link rel="stylesheet" type='text/css' href="/_next/static/style.css" />
-          <link rel='stylesheet' type='text/css' href='/static/css/nprogress.css' />
-          <link rel='stylesheet' type='text/css' href='/static/css/Trirong.css' />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta name="theme-color" content="#0D47A1" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
+        <link rel='stylesheet' type='text/css' href='/static/css/nprogress.css' />
+        <link rel='stylesheet' type='text/css' href='/static/css/Trirong.css' />
+
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content="IT, developer, programmer" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/static/icons/site.webmanifest" />
+        <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color={theme_color} />
+        <link rel="shortcut icon" href="/static/icons/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="Technukrom" />
+        <meta name="application-name" content="Technukrom" />
+        <meta name="msapplication-TileColor" content={theme_color} />
+        <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
+        <meta name="theme-color" content={theme_color} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      </html >
     )
   }
 }
